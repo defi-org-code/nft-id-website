39cba92f7466984f57d9875095ec75d06ad0af7f
denis-mixin <denisn@mixin.co.il>
2021-09-22 13:14:56 +0300

    small ui fix

--- src/components/Certificate/index.tsx
+++ src/components/Certificate/index.tsx
@@ -6,21 +6,16 @@ import Content from "./Content";
 interface IProps {
   certificate: ICertificate | null;
   isLoading?: boolean;
+  notVarified?: boolean;
 }
 
-const isVerified = (certificate: ICertificate | null) => {
-  if (certificate) {
-    return certificate.twitter_user_info.verified;
-  }
-  return false;
-};
-
-function Cretificate({ certificate, isLoading }: IProps) {
+function Cretificate({ certificate, isLoading, notVarified }: IProps) {
   const className = isLoading
     ? "certificate certificate-loading"
-    : isVerified(certificate)
-    ? "certificate"
-    : "certificate certificate-not-verified";
+    : notVarified
+    ? "certificate certificate-not-verified"
+    : "certificate";
+
   return (
     <div className={className}>
       {isLoading ? (
@@ -35,7 +30,7 @@ function Cretificate({ certificate, isLoading }: IProps) {
         />
       ) : (
         <>
-          {!isVerified(certificate) && (
+          {notVarified && (
             <img
               src={images.notVerified}
               alt="not verified"
