cce580028af19c7f451c7bcd37a4282ad22e5ba6
denis-mixin <denisn@mixin.co.il>
2021-09-22 15:22:10 +0300

    a

--- src/consts/index.ts
+++ src/consts/index.ts
@@ -1,7 +1,7 @@
 export const routes = {
   home: "/",
-  asset: ["/nft-id-website/:twitterHandle", "/:contractAddress/:tokenId"],
-  verify: "/nft-id-website/verify",
+  asset: ["/:twitterHandle", "/:contractAddress/:tokenId"],
+  verify: "/verify",
 };
 
 export const OPEN_SEA_ASSETS_URL = "https://opensea.io/assets";
