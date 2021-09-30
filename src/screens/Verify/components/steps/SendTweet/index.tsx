b0e7ef47854dbaef9c1bde889c97117f2859a17b
Rotem Yakir <yakir.rotem@gmail.com>
2021-09-30 10:28:30 +0300

    Change tweet

--- src/screens/Verify/components/steps/SendTweet/index.tsx
+++ src/screens/Verify/components/steps/SendTweet/index.tsx
@@ -28,7 +28,7 @@ function SendTweet() {
   const pollInterval = useRef<any>(null);
   const sendTweet = () => {
     const params = encodeURIComponent(
-      `I just claimed my NFT certificate for owning ${openSeaUrl} @mynft_fyi #mynftfyi\n\nCheck out my certificate: https://www.mynft.fyi/${twitterHandle} `
+      `I just claimed my NFT certificate for owning ${openSeaUrl} @mynft_fyi #mynftfyi\n\nCheck out my certificate: https://www.mynft.fyi/${twitterHandle} 👇`
     );
     window.open(`https://twitter.com/intent/tweet?text=${params}`);
     setVerificationPending(true);
